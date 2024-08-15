85ce573df3c622b1fb74e6c28412e99580fbcfcd
tunaunnie <ygw021227@gmail.com>
2024-08-15 11:13:45 +0900

    0815

--- step6_advanced1/boj25206.cpp
+++ step6_advanced1/boj25206.cpp
@@ -15,13 +15,14 @@ int main(){
 
     for(int i = 0; i<20; i++){
         std::cin >> subject >> num >> grade;
-        printf("subject: %s, num: %lf, grade: %s\n", subject, num, grade);
-        //scanf("%s %f %s", &subject, &num, grade);
 
-        num_sum += num;
+
 
         int isP = grade.compare("P");
         if (isP != 0){
+
+            num_sum += num;
+            
             if(grade == "A+"){
                 total_sum += 4.5 * num;
             } else if (grade == "A0"){
@@ -38,7 +39,7 @@ int main(){
                 total_sum += 1.5 * num;
             } else if (grade == "D0"){
                 total_sum += 1.0 * num;
-            } else {
+            } else if (grade == "F") {
                 total_sum += 0.0 * num;
             }
         } 
--- step6_advanced1/boj2941.cpp
+++ step6_advanced1/boj2941.cpp
@@ -6,52 +6,45 @@ using namespace std;
 
 int main(){
 
-    char *words[12] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=", "a", "e", "k"};
+    char *words[11] = {"a", "e", "k", "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
-    string str;
-    getline(cin, str);
+    std::string input;
+    std::getline(std::cin, input);
 
-    int length = 0;
-    while(str[length] != '\0'){
-        length++;
-    }
-
-    int index = 0;
+    int length = input.length();
     int wordcount = 0;
 
-        for(int i = 0; i<11; i++){
-            printf("finding word %s ...\n", words[i]);
 
-            //찾았으면 찾은 위치, 못찾았으면 -1
-            int temp;
-            temp = str.find(words[i], index);
+    //temp는 각 알파벳의 시작 위치
+    int temp = 0;
+    int result = 0;
 
-            if(temp != -1) {
-                printf("찾은 단어: %s, 찾은 위치: %d\n", words[i], temp);
-                
-                if(i==0 || i==1 || i==3 || i==6 || i==7 ){
-                    wordcount += 1;
-                    index += 2;
-                }
-                else if (i==2){
-                    wordcount += 1;
-                    index += 3;
-                }
-                else if (i==4 || i==5){
-                    wordcount += 1;
-                    index += 2;
-                }
-                else if (i==8 || i==9 || i==10){
-                    wordcount += 1;
-                    index += 1;
-                }
-            } 
+    //한글자 앞으로 갈때마다 단어 있나 확인
+    for (temp; temp<length; ){
 
-            if(index >= length) break;
+        if(temp>length) break;
+
+        for(int j = 0; j<11; j++){
+
+            if (j==0 || j==1|| j==2) {
+                result = input.compare(temp, 1, words[j]);
+                if(result == 0) { wordcount += 1; temp+=1;  break;} 
+            }
+            else if (j==5){
+                result = input.compare(temp, 3, words[j]);
+                if(result == 0) { wordcount += 1; temp+=3;  break;} 
+            }
+            else {
+                result = input.compare(temp, 2, words[j]);
+                if(result == 0) { wordcount += 1; temp+=2;  break;} 
+            }
         }
+        if(result != 0) { wordcount += 1; temp+=1;}
 
+    }
 
-   printf("wordcount: %d\n", wordcount);
+    printf("%d", wordcount);
+
 
     return 0;
 }
@@ -59,6 +52,19 @@ int main(){
 
 /*
 
+ljes=njak
+
+1. 한 글자씩 돌면서 찾아
+  2. 모든 단어 순회하다가
+     lj 찾았어 -> 카운트 증가하고 2부터
+  2. 모든 단어 순회하다가
+
+lj (뭐라도 일단) 찾았으면 -> 그 temp 더한 위치에서부터 다시 word[1]부터 찾기.
+한 번 찾은 위치는 다시 돌아가지 않는 게 중요
+
+a
+e
+k
 c=
 c-
 dz=
@@ -67,5 +73,6 @@ lj
 nj
 s=
 z=
+
 
 */
