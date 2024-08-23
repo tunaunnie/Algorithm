fa63d28eb06276775ca7f56c2836e796177f54c1
tunaunnie <ygw021227@gmail.com>
2024-08-23 22:52:43 +0900

    0823

--- step8_math1/boj1193.cpp
+++ step8_math1/boj1193.cpp
@@ -2,30 +2,36 @@
 
 int main(){
 
+    long long input;
+    scanf("%lld", &input);
 
+    int temp = 0;
+    int m;
+
+    /*
+    temp: 0 -> 0+1
+    */
+    for(int i = 1; ; i++){
+        temp += i;
+        
+        if(input <= temp){
+            m = i;
+            break;
+        }
+    }
+
+    int sum = 0;
+    for(int x = 1; x<m; x++){
+        sum += x;
+    }
+    int rest = input - sum;
+
+    if (m % 2 == 0){
+        printf("%d/%d", rest, m-rest+1);
+
+    } else {
+        printf("%d/%d", m-rest+1, rest);
+    }
 
     return 0;
 }
-
-
-/*
-
-1, 1
-1, 2 / 2, 1 (상행)
-3, 1 / 2, 2 / 1, 3 (하행)
-1, 4 / 2, 3 / 3, 2 / 4, 1/ (상행)
-
-
-answer[] 배열에 저장. 공간 할당은 .. 하나 늘때마다 realloc할까?
-
-전체 개수는 계속 세고 있기
-
-or 1, 2, 3, 4, ... 로 더해서 n번째 되는 loop 계산할까?
-
-n(1+n)/2 = input
-방정식 풀어야되네..
-
-
-
-
-*/
